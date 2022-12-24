b99a2fd24c96fb11b6077fe021161f700f940560
ma-mahmudi <ma-mahmudi18@outlook.com>
2022-12-24 16:23:49 +0330

    Push

--- src/components/view/main/main.js
+++ src/components/view/main/main.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Main = () => {
     return (
         <div>
-            <div dir={"rtl"} className={"h-screen w-full"}>
+            <div dir={"rtl"} className={"h-screen w-full background-black"}>
+              <span>asf</span>
+
             </div>
         </div>
     );
